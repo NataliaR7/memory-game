3662ccdbacc5c6d2d6e575ce1e2209e75a98da08
Natalia <natasha.rogachyova@mail.ru>
2020-11-10 15:03:19 +0500

    setting up a webpack and gitignore

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import style from './style.css';
+import style2 from './style2.css';
+alert('11');
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/i,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    }
+};
