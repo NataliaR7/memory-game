98e56c18651fa8a40cc1648d2a7ba2cabdcdaf42
Natalia <natasha.rogachyova@mail.ru>
2020-11-17 19:11:42 +0500

    layout leaderboard

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -1,5 +1,21 @@
+import record from './record'
+
+const recordCount = 20;
 export default function leaderboard() {
-    return '<h1>EEEEEEEEEEEE</h1>';
+    let records = [];
+    for(let i = 1; i < 20; i++) {
+        records.push(record(i, `User ${i}`, 400 - i));
+    }
+    records[0] = wrapRecords(records[0], 'firstPlace');
+    records[1] = wrapRecords(records[1], 'secondPlace');
+    records[2] = wrapRecords(records[2], 'thirdPlace');
+    return `
+    <div class="head">Leaderboard</div>
+    <div class="body">${records.join('')}</div>`;
+}
+
+function wrapRecords(record, id) {
+    return `<div id="${id}">${record}</div>`
 }
 
 
---
+++ src/components/leaderboard/record.js
@@ -0,0 +1,12 @@
+export default function record(number, userName, point) {
+    return `
+    <div class = 'record'>
+        <div class = "number"><span>${number}</span></div>
+        <div class = 'userName'>${userName}</div>
+        <div class = 'point'>
+            <span>${point}</span>
+            <span>point<span>
+        </div>
+    </div>
+    `;
+}
